1256f81f2116ebb2632b0265bbcf8385f26b3c10
rhimmelbauer <roberto.himmelbauer@codeandmodules.com>
2017-12-19 09:12:10 -0800

    Stated the structure of the base and the generic screens. Next step import the model

--- apptoSolution/urls.py
+++ apptoSolution/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+
+from smartAtomizer import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	url(r'^$', views.home, name='dashboard'),
+	url(r'^clients', views.clients, name='clients'),
+	url(r'^zones', views.zones, name='zones'),
+	url(r'^smart_atomizers', views.smart_atomizers, name='smart_atomizers'),
+    url(r'^admin/', admin.site.urls),
 ]
--- smartAtomizer/views.py
+++ smartAtomizer/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+
+def home(request):
+	return render(request, 'dashboard.html')
+
+def clients(request):
+	return render(request, 'clients.html')
+
+def zones(request):
+	return render(request, 'zones.html')
+
+def smart_atomizers(request):
+	return render(request, 'smart_atomizers.html')
